d5afae1fe9e97ff30e2fcc31450872152b4fb8eb
Flavio <chiarappa_flavio.com>
2024-05-10 16:47:12 +0200

    speriamo in bene

--- script.js
+++ script.js
@@ -60,16 +60,68 @@ function calc() {
     
     try {
         display.value = eval(display.value);
-    } catch {
-        display.value = "ERROR";
-       
-        setTimeout (() => {
-            del();
-        },2000);
-    }
+    } catch (error){
         
+        error();
+        console.error(error);
+    }
+}
+
+function error() {
+    display.value = "ERROR";
+
+
+    setTimeout(() => {
+        del();
+    }, 2000);
+}
+
+function goniometricOperator(operator) {
+
+    if (isNaN(display.value)) {
+        error();
+    } else {
+        let value = display.value;
+        value = parseInt(value);  
+        identifyOperator(operator, value);
+    }
+
+
 }
 
 function delChar() {
     display.value = display.value.slice(0, -1);
+}
+
+
+function identifyOperator(selectedOperator, value) {
+    let conversion;
+    console.log(selectedOperator);
+    switch (selectedOperator) {
+        case "sin":
+            conversion = Math.sin(value * Math.PI / 180);
+            // console.log("here sine " + value);
+            checkGoniometricOperator(value);
+            break;
+        case "cos":
+            conversion = Math.cos(value * Math.PI / 180);
+            // console.log("here cos " + value);
+            checkGoniometricOperator(value);
+            break;
+        case "tan":
+            conversion = Math.tan(value * Math.PI / 180);
+            // console.log("here tan " + value);
+            checkGoniometricOperator(value);
+            break;
+
+    }
+}
+
+function checkGoniometricOperator(value){
+
+    if (value === 0 || value === 90 || value === 180 || value === 270 || value === 360)
+        display.value = value.toFixed();
+    else
+        display.value = value.toFixed(2);
+
 }
