90a6fb7f77038db4b46e772a0a01b99c03ee502a
Flavio <chiarappa_flavio.com>
2024-05-14 12:46:33 +0200

    modificaro html e js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function fError() {
 
     setTimeout(() => {
         del();
-    }, 2000);
+    }, 1000);
 }
 
 function append(input) {
@@ -76,55 +76,50 @@ function calc() {
 }
 
 
-
-function goniometricOperator(operator) {
-
-    if (isNaN(display.value)) {
-        fError();
-    } else {
-        let value = display.value;
-        value = parseInt(value);  
-        identifyOperator(operator, value);
-    }
-
-
-}
-
 function delChar() {
     display.value = display.value.slice(0, -1);
 }
 
 
-function identifyOperator(selectedOperator, value) {
+function identifyOperator(selectedOperator) {
+
     let conversion;
+    let value = display.value;
+
+    if (isNaN(value) || value === "") {
+        console.log("error")
+        fError();
+        return;
+    } 
+    
+    value = parseInt(value);
+
+    
     console.log(selectedOperator);
     switch (selectedOperator) {
 
         case "sin":
+            console.log("entaro in case sin");
             conversion = Math.sin(value * Math.PI / 180);
             // console.log("here sine " + value);
-            checkGoniometricOperator(value);
+            checkGoniometricOperator(value, conversion);
             break;
         
         case "cos":
             conversion = Math.cos(value * Math.PI / 180);
             // console.log("here cos " + value);
-            checkGoniometricOperator(value);
+            checkGoniometricOperator(value, conversion);
             break;
         
         case "tan":
             conversion = Math.tan(value * Math.PI / 180);
+            console.log(conversion);
             // console.log("here tan " + value);
-            checkGoniometricOperator(value);
+            checkGoniometricOperator(value, conversion);
             break;
         
-        case "e^":
-            console.log(value);
-            display.value = Math.exp(value);
-            
-            break;
-        
-        // case "":
+        case "%":
+            display.value = value % 2;
 
         //     break;
         
@@ -139,11 +134,11 @@ function identifyOperator(selectedOperator, value) {
     }
 }
 
-function checkGoniometricOperator(value){
+function checkGoniometricOperator(value, conversion){
 
     if (value === 0 || value === 90 || value === 180 || value === 270 || value === 360)
-        display.value = value.toFixed();
+        display.value = conversion.toFixed();
     else
-        display.value = value.toFixed(2);
+        display.value = conversion.toFixed(2);
 
 }
